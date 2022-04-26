d74f6d03d1b3af03f70dc000330932e23a47a12f
Paolo Castro <paolo.castro@colorado.edu>
2022-04-26 10:45:42 -0600

    Starting making classes

--- main.py
+++ main.py
@@ -9,6 +9,46 @@ OUTPUT_VIDEO = os.path.join(OUTPUT_DIR, "out.mp4")
 SIZE = (2000, 2000)
 DEFAULT_BACKGROUND = (255, 255, 255)
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+TRANSPARENT = (np.nan, np.nan, np.nan)
+
+
+class Video:
+    def __init__(self, output_file, frames_per_second, dimensions, file_format="mp4v", background_color=BLACK):
+        self.dimensions = dimensions
+        self.video = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*file_format), frames_per_second, dimensions)
+        self.empty_frame = self._get_empty_frame_template(dimensions, background_color)
+        self.frames = []
+
+    @staticmethod
+    def _get_empty_frame_template(dimensions, background_color):
+        frame = np.zeros((dimensions[0], dimensions[1], 3), np.uint8)
+        if background_color != BLACK:
+            frame[:] = background_color
+        return frame
+
+    def render(self):
+        for x in range(self.dimensions[0]):
+            for y in range(self.dimensions[1]):
+                for frame in reversed(self.frames):
+                    pass
+
+
+class Frame:
+    def __init__(self, dimensions):
+        self.dimensions = dimensions
+        self.initial_frame = self._get_initial_frame(dimensions)
+
+    @staticmethod
+    def _get_initial_frame(dimensions):
+        frame = np.zeros((dimensions[0], dimensions[1], 3), np.uint8)
+        frame[:] = TRANSPARENT
+        return frame
+
+    def render(self, x, y):
+        return self.initial_frame[x][y]
+
 
 def gen_img(i):
     img = np.zeros((SIZE[0], SIZE[1], 3), np.uint8)
