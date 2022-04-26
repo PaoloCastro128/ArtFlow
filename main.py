052b0babe48e5cd20c54a0baad018081fa389f83
Paolo Castro <paolo.castro@colorado.edu>
2022-04-26 16:43:07 -0600

    More testing

--- main.py
+++ main.py
@@ -51,9 +51,10 @@ class Video:
 
         print("Starting render...")
         for i in range(length_frames):
-            if verbose_frames > 0 and i % verbose_frames:
+            if verbose_frames > 0 and i % verbose_frames == 0:
                 print(f"\rRendering: frame={i}/{length_frames} -- "
-                      f"seconds={round(i/self.frames_per_second, 2)}/{round(length_frames/self.frames_per_second, 2)}", end="")
+                      f"seconds={round(i/self.frames_per_second, 2)}/{round(length_frames/self.frames_per_second, 2)}",
+                      end="")
             self.video.write(self.render_frame(i))
         print(f"\rRendering: frame={length_frames}/{length_frames} -- "
               f"seconds={round(length_frames / self.frames_per_second, 2)}/"
@@ -63,9 +64,9 @@ class Video:
 
 
 class Layer:
-    def __init__(self, opacity=1, gamma=0):
-        self.opacity = opacity
-        self.gamma = gamma
+    def __init__(self, opacity=1.0, gamma=0.0):
+        self.opacity = self.get_dynamic_value(opacity)
+        self.gamma = self.get_dynamic_value(gamma)
 
     @abc.abstractmethod
     def get_overlay(self, base_frame, frame_count):
@@ -79,20 +80,23 @@ class Layer:
 
     def render(self, base_frame, frame_count):
         overlay = self.get_overlay(base_frame.copy(), frame_count)
-        return cv2.addWeighted(overlay, self.opacity, base_frame, 1 - self.opacity, self.gamma)
+        return cv2.addWeighted(overlay, self.opacity.get(frame_count), base_frame,
+                               1 - self.opacity.get(frame_count), self.gamma.get(frame_count))
 
 
 class Circle(Layer):
-    def __init__(self, center_x, center_y, radius, color, opacity=1):
+    def __init__(self, center_x, center_y, radius, color, thickness=-1, opacity=1.0):
         self.center_x = self.get_dynamic_value(center_x)
         self.center_y = self.get_dynamic_value(center_y)
+        self.thickness = self.get_dynamic_value(thickness)
         self.radius = self.get_dynamic_value(radius)
         self.color = self.get_dynamic_value(color)
         super().__init__(opacity=opacity)
 
     def get_overlay(self, base_frame, frame_count):
-        center = (self.center_x.get(frame_count), self.center_y.get(frame_count))
-        cv2.circle(base_frame, center, self.radius.get(frame_count), self.color.get(frame_count), thickness=-1)
+        center = (int(self.center_x.get(frame_count)), int(self.center_y.get(frame_count)))
+        cv2.circle(base_frame, center, int(self.radius.get(frame_count)), self.color.get(frame_count),
+                   thickness=int(self.thickness.get(frame_count)))
         return base_frame
 
 
@@ -118,15 +122,26 @@ class DynamicValue:
         return value
 
 
+def make_circle(color, offset):
+    center_x = DynamicValue(1000)
+    center_x.add_updater(lambda val, frame_count: val + 400 * math.cos(frame_count/36 + offset))
+    center_y = DynamicValue(1000)
+    center_y.add_updater(lambda val, frame_count: val - 400 * math.sin(frame_count/36 + offset))
+    circle = Circle(center_x, center_y, 500, color, opacity=0.3333)
+    return circle
+
+
 def main():
-    video = Video(OUTPUT_VIDEO, 20, SIZE)
-    radius = DynamicValue(1)
-    radius.add_updater(lambda val, frame_count: val + frame_count*4)
-    center_x = DynamicValue(500)
-    center_x.add_updater(lambda val, frame_count: val - 4 * math.cos(frame_count/3))
-    circle = Circle(center_x, 1000, radius, (120, 200, 20))
-    video.add_layer(circle)
-    video.render(length_seconds=5)
+    video = Video(OUTPUT_VIDEO, 20, SIZE, background_color=WHITE)
+
+    circle1 = make_circle((255, 255, 0), 0)
+    circle2 = make_circle((0, 255, 255), math.pi*2/3)
+    circle3 = make_circle((255, 0, 255), math.pi*4/3)
+
+    video.add_layer(circle1)
+    video.add_layer(circle2)
+    video.add_layer(circle3)
+    video.render(length_seconds=8)
 
 
 if __name__ == '__main__':
