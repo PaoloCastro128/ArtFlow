6d0ae9967a27b95b8f5f49074c43e71d838a7bb5
Paolo Castro <paolo.castro@colorado.edu>
2022-04-26 10:02:29 -0600

    Animation

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import os
-from PIL import Image, ImageDraw
+import cv2
+import numpy as np
 
 OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output")
-OUTPUT_FILE = os.path.join(OUTPUT_DIR, "out.png")
+OUTPUT_IMG = os.path.join(OUTPUT_DIR, "out.png")
+OUTPUT_VIDEO = os.path.join(OUTPUT_DIR, "out.mp4")
 
-SIZE = (1000, 1000)
+SIZE = (2000, 2000)
 DEFAULT_BACKGROUND = (255, 255, 255)
 
 
+def gen_img(i):
+    img = np.zeros((SIZE[0], SIZE[1], 3), np.uint8)
+    # img.fill(255)
+    cv2.circle(img, (1000, 1000), i*4+1, (120, 50, 150), thickness=-1)
+    return img
+
+
 def main():
-    img = Image.new("RGB", SIZE, DEFAULT_BACKGROUND)
-    draw = ImageDraw.Draw(img)
+    video = cv2.VideoWriter(OUTPUT_VIDEO, cv2.VideoWriter_fourcc(*"mp4v"), 20, SIZE)
 
-    draw.ellipse([(400, 400), (600, 600)], fill=(120, 50, 150))
-    print(img.getpixel((500, 500)))
-    print(img.getpixel((0, 500)))
-    draw.point((0, 500), (0, 0, 0))
-    print(img.getpixel((0, 500)))
+    for i in range(20*10):
+        if (i+1) % 20 == 0:
+            print(f"\rRendering frame {i+1}/{20*10}", end="")
+        video.write(gen_img(i))
 
-    img.save(OUTPUT_FILE)
+    print(f"\nDone")
+
+    video.release()
 
 
 if __name__ == '__main__':
