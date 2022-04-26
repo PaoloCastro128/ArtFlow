246f7cfe73fdd9a6f5c6a44f4d87d3f5c9d377f1
Paolo Castro <paolo.castro@colorado.edu>
2022-04-26 13:28:01 -0600

    Base for animation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import abc
 import os
 import cv2
 import numpy as np
@@ -17,9 +18,14 @@ TRANSPARENT = (np.nan, np.nan, np.nan)
 class Video:
     def __init__(self, output_file, frames_per_second, dimensions, file_format="mp4v", background_color=BLACK):
         self.dimensions = dimensions
-        self.video = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*file_format), frames_per_second, dimensions)
+        self.frames_per_second = frames_per_second
+        self.video = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*file_format),
+                                     self.frames_per_second, self.dimensions)
         self.empty_frame = self._get_empty_frame_template(dimensions, background_color)
-        self.frames = []
+        self.layers = []
+
+    def add_layer(self, layer):
+        self.layers.append(layer)
 
     @staticmethod
     def _get_empty_frame_template(dimensions, background_color):
@@ -28,46 +34,89 @@ class Video:
             frame[:] = background_color
         return frame
 
-    def render(self):
-        for x in range(self.dimensions[0]):
-            for y in range(self.dimensions[1]):
-                for frame in reversed(self.frames):
-                    pass
+    def render_frame(self, frame_count):
+        current_frame = self.empty_frame.copy()
+        for layer in self.layers:
+            current_frame = layer.render(current_frame, frame_count)
+        return current_frame
+
+    def render(self, length_frames=None, length_seconds=None, verbose_frames=None):
+        if length_frames is None and length_seconds is None:
+            raise RuntimeError("Must provide length_frames or length_seconds.")
+        if length_frames is None and length_seconds:
+            length_frames = int(length_seconds * self.frames_per_second)
+        if verbose_frames is None:
+            verbose_frames = self.frames_per_second
+
+        print("Starting render...")
+        for i in range(length_frames):
+            if verbose_frames > 0 and i % verbose_frames:
+                print(f"\rRendering: frame={i}/{length_frames} -- "
+                      f"seconds={round(i/self.frames_per_second, 2)}/{round(length_frames/self.frames_per_second, 2)}", end="")
+            self.video.write(self.render_frame(i))
+        print(f"\rRendering: frame={length_frames}/{length_frames} -- "
+              f"seconds={round(length_frames / self.frames_per_second, 2)}/"
+              f"{round(length_frames/self.frames_per_second, 2)}", end="")
+        print("\nRender done!")
+        self.video.release()
 
 
-class Frame:
-    def __init__(self, dimensions):
-        self.dimensions = dimensions
-        self.initial_frame = self._get_initial_frame(dimensions)
+class Layer:
+    def __init__(self, opacity=1, gamma=0):
+        self.opacity = opacity
+        self.gamma = gamma
+
+    @abc.abstractmethod
+    def get_overlay(self, base_frame, frame_count):
+        pass
 
     @staticmethod
-    def _get_initial_frame(dimensions):
-        frame = np.zeros((dimensions[0], dimensions[1], 3), np.uint8)
-        frame[:] = TRANSPARENT
-        return frame
+    def get_dynamic_value(val):
+        if isinstance(val, DynamicValue):
+            return val
+        return DynamicValue(val)
 
-    def render(self, x, y):
-        return self.initial_frame[x][y]
+    def render(self, base_frame, frame_count):
+        overlay = self.get_overlay(base_frame.copy(), frame_count)
+        return cv2.addWeighted(overlay, self.opacity, base_frame, 1 - self.opacity, self.gamma)
 
 
-def gen_img(i):
-    img = np.zeros((SIZE[0], SIZE[1], 3), np.uint8)
-    # img.fill(255)
-    cv2.circle(img, (1000, 1000), i*4+1, (120, 50, 150), thickness=-1)
-    return img
+class Circle(Layer):
+    def __init__(self, center_x, center_y, radius, color, opacity=1):
+        self.center_x = self.get_dynamic_value(center_x)
+        self.center_y = self.get_dynamic_value(center_y)
+        self.radius = self.get_dynamic_value(radius)
+        self.color = self.get_dynamic_value(color)
+        super().__init__(opacity=opacity)
+
+    def get_overlay(self, base_frame, frame_count):
+        center = (self.center_x.get(frame_count), self.center_y.get(frame_count))
+        cv2.circle(base_frame, center, self.radius.get(frame_count), self.color.get(frame_count), thickness=-1)
+        return base_frame
+
+
+class DynamicValue:
+    def __init__(self, base_value):
+        self.base_value = base_value
+        self.updaters = []
+
+    def add_updater(self, updater):
+        self.updaters.append(updater)
+
+    def get(self, frame_count):
+        value = self.base_value
+        for updater in self.updaters:
+            value = updater(value, frame_count)
+        return value
 
 
 def main():
-    video = cv2.VideoWriter(OUTPUT_VIDEO, cv2.VideoWriter_fourcc(*"mp4v"), 20, SIZE)
-
-    for i in range(20*10):
-        if (i+1) % 20 == 0:
-            print(f"\rRendering frame {i+1}/{20*10}", end="")
-        video.write(gen_img(i))
-
-    print(f"\nDone")
-
-    video.release()
+    video = Video(OUTPUT_VIDEO, 20, SIZE)
+    radius = DynamicValue(1)
+    radius.add_updater(lambda val, i: val + i*4)
+    circle = Circle(1000, 1000, radius, (120, 200, 20))
+    video.add_layer(circle)
+    video.render(length_seconds=5)
 
 
 if __name__ == '__main__':
